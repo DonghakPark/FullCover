df138a38d4f68d5dff0b7de518448338c76ed367
Donghak PARK <donghark03@naver.com>
2020-05-18 12:42:34 +0900

    2020-05-18
    gas collect code change

---
+++ Gas_data_collect/fire_server.py
@@ -0,0 +1,50 @@
+import sys
+from socket import *
+from gasLeakage import *
+import pandas as pd
+import datetime
+import time
+
+def main():
+
+    ''' print("[DEBUG] Connect Server Start")
+
+    SERVER_ADDR = '192.168.0.101'
+    PORT = 1626
+    BUFSIZE = 1024
+
+    client_socket = socket(AF_INET, SOCK_STREAM)
+    client_socket.connect((SERVER_ADDR, PORT))
+    '''
+    
+    gas = gasLeakage()
+
+    while True:
+        '''server_recv = client_socket.recv(BUFSIZE)
+
+        if server_recv == -1:
+            print("[DEBUG] Exit")
+            break;
+        '''
+        
+        getData = gas.getSensorData()
+        gas.controlFan(getData)
+
+        '''
+        sensor_data = bytes(getData)
+
+        client_socket.send(sensor_data)
+        '''
+        print("[DEBUG] Smoke Sensor Value = %u"%(getData))
+        
+        data = [[datetime.datetime.now(), getData]]
+        submission = pd.DataFrame(data)
+        submission.to_csv('./Gas_DataSet.csv', header = False, mode = 'a', index = False)
+        time.sleep(0.5)
+        
+
+    print("END")
+
+
+if __name__ == "__main__":
+    main()
