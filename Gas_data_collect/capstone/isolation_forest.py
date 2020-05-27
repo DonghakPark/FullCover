b14e41a8dd6187b5b2e71b84c4dd66219c9dc373
DonghakPark <donghark03@naver.com>
2020-05-27 22:14:51 +0900

    2020-05-27

--- Gas_data_collect/capstone/isolation_forest.py
+++ Gas_data_collect/capstone/isolation_forest.py
@@ -1,10 +1,33 @@
+import pandas as pd
 from sklearn.ensemble import IsolationForest
 
-clf = IsolationForest(n_estimators=50, max_samples=50,
-                      contamination=float(0.004),
-                      max_features=1.0,
-                      bootstrap=False,
-                      n_jobs=-1.
-                      random_state = None,
-                      verbose =0,
-                      behaviour = "new")
+data = pd.read_csv("./GAS_DATASET.csv")
+#데이터 로드
+
+clf=IsolationForest(n_estimators=50, max_samples=50, contamination=float(0.004),
+                        max_features=1.0, bootstrap=False, n_jobs=-1, random_state=None, verbose=0,behaviour="new")
+clf.fit(data)
+pred = clf.predict(data)
+data['anomaly'] = pred
+outliers = data.loc[data['anomaly'] == -1]
+outlier_index = list(outliers.index)
+print(data['anomaly'].value_counts())
+
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from mpl_toolkits.mplot3d import Axes3D
+
+pca = PCA(n_components=3)
+scaler = StandardScaler()
+
+#정규화 실시
+
+X = scaler.fit_transform(data)
+X_reduce = pca.fit_transform(X)
+fig = plt.figure()
+# Plot the compressed data points
+fig.scatter(X_reduce[:, 0], X_reduce[:, 1], s=4, lw=1, label="inliers",c="green")
+fig.scatter(X_reduce[outlier_index,0],X_reduce[outlier_index,1],lw=2, s=60, marker="x", c="red", label="outliers")
+fig.legend()
+plt.show()
--- User_GUI/GUI.py
+++ User_GUI/GUI.py
@@ -1,19 +1,30 @@
 import sys
-from PyQt5.Qtwidgets import QApplication, Qwidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QListView
+from PyQt5.QtGui import QIcon
 
-class Myapp(Qwidget):
+class MyApp(QWidget):
 
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
-        self.setWindowTitle(" Full_Cover ")
-        self.move(300,300)
-        self.resize(400, 200)
+
+        btn1 = QPushButton('차량 번호 등록', self)
+        btn1.setGeometry(30, 100, 180, 60)
+
+        btn2 = QPushButton("차량 인식", self)
+        btn2.setGeometry(30, 200, 180, 60)
+
+        Carnum = QListView(self)
+        self.setWindowTitle("Car_Number_recognizer")
+        self.setWindowIcon(QIcon('./dku_logo.png'))
+        self.setGeometry(300,300,1000,600)
         self.show()
+
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    wx = Myapp()
+    ex = MyApp()
     sys.exit(app.exec_())
